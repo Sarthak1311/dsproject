1c3f874fa78f28585a4c34a6f6bb7dbc45669299
sarthaktyagi1704@gmail.com <sarthaktyagi1704@gmail.com>
2024-10-18 23:07:56 +0530

    training model

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from src.mlproject.Logger import logging
 from src.mlproject.components.Data_injestion import DataIngestion
 from src.mlproject.components.Data_injestion import DataIngestoinConfig
 from src.mlproject.components.Data_transformation import DataTransformation,DataTransformationConfig
+from src.mlproject.components.Model_trainer import ModelTrainer
 import pandas as pd
 
 
@@ -16,7 +17,12 @@ if __name__=="__main__":
         train_data_path ,test_data_path = data_ingestion.initiate_data_ingestion()
         # data_transformation_config = DataTransformationConfig()
         data_transforamtion = DataTransformation()
-        data_transforamtion.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr,test_arr,_ = data_transforamtion.initiate_data_transormation(train_data_path,test_data_path)
+
+        #model training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
 
